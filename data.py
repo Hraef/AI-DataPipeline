e206237976ba2e4cb36aebe91f1402eb602a528e
Harmon <harmonraef@gmail.com>
2025-06-10 16:42:37 -0500

    test push

--- data.py
+++ data.py
@@ -1,6 +1,3 @@
-from newsapi import NewsApiClient
-
-#sources = newsapi.get_sources()
 import json
 from newsapi import NewsApiClient
 from dotenv import load_dotenv
@@ -15,10 +12,4 @@ newsapi = NewsApiClient(api_key=api_key)
 # /v2/top-headlines
 top_headlines = newsapi.get_top_headlines(language='en',country='us')
 
-articles = top_headlines.get('articles', [])
-titles = [article['title'] for article in articles]
-
-for i, title in enumerate(titles, 1):
-    print(f"{i}. {title}")
-
-
+df = df[]
