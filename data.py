8d892a25ab0150428b405f9a51ef305e92111246
Harmon <harmonraef@gmail.com>
2025-06-10 21:05:19 -0500

    added data cleaning using pandas

--- data.py
+++ data.py
@@ -1,10 +1,9 @@
-from newsapi import NewsApiClient
-
-#sources = newsapi.get_sources()
 import json
 from newsapi import NewsApiClient
 from dotenv import load_dotenv
 import os
+import pandas as pd
+
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
@@ -15,10 +14,22 @@ newsapi = NewsApiClient(api_key=api_key)
 # /v2/top-headlines
 top_headlines = newsapi.get_top_headlines(language='en',country='us')
 
-articles = top_headlines.get('articles', [])
-titles = [article['title'] for article in articles]
 
-for i, title in enumerate(titles, 1):
-    print(f"{i}. {title}")
+#df = pd.DataFrame(top_headlines)
+#df.to_csv('output.csv', index=False)
+articles = top_headlines.get('articles', [])
+df = pd.DataFrame([{
+    'title': a.get('title'),
+    'description': a.get('description'),
+    'content': a.get('content'),
+    'source': a.get('source', {}).get('name'),
+    'publishedAt': a.get('publishedAt')
+    } for a in articles])
+
+df.to_csv('output.csv', index=False)
+#titles = [article['title'] for article in articles]
+
+#for i, title in enumerate(titles, 1):
+#    print(f"{i}. {title}")
 
 
